a30fdf3948877ea847bb645a58b1a0be386d8cc3
Omer Lev-Ron <omeround3@gmail.com>
2022-01-22 21:52:59 +0200

    OpenMP working

--- sequence_alignment.c
+++ sequence_alignment.c
@@ -1,13 +1,13 @@
-/* 
+/*
 	This program will calculate the Sequence Alignment scores of sequences in the input (input file). The calculation proceess involves MPI, OpenMP and CUDA.
 
-	Sequence Alignment – Estimatation of similarity of two strings of letters 
+	Sequence Alignment – Estimatation of similarity of two strings of letters
 	Sequence - a string of capital letters
 	Alignment Score - The score given to comparision between two sequences. The higher the value, the more similar the sequences.
 
 	Input: letters sequences for comparision
 	Output: For each sequence, the offset (n) and hypen location in the mutant sequence (k) which procudes maxial alignment score when comparing to the first sequence
-	
+
 	Author:
 	Omer Lev-Ron
 */
@@ -32,8 +32,8 @@ char chars_comparision[CHARS][CHARS]; /* table for storing characters comparisio
 int main(int argc, char *argv[])
 {
 	int num_of_sequences;
-	int procceses_amount;				/* number of processes*/
-	int process_rank;					/* process rank (process ID) */
+	int procceses_amount; /* number of processes*/
+	int process_rank;	  /* process rank (process ID) */
 	int work_size;
 	int offset_remainder;
 	int process_start_offset, process_end_offset;
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	int omp_start_offset, omp_end_offset;
 	int cuda_start_offset, cuda_end_offset;
 
-	MPI_Status status;					/* return status for receive */
+	MPI_Status status; /* return status for receive */
 
 	/* Initialize MPI */
 	MPI_Init(&argc, &argv);							  /* send MPI command line arguments  */
@@ -57,12 +57,13 @@ int main(int argc, char *argv[])
 	/* Check if number of processes is 2 or more */
 	if (procceses_amount < 2)
 	{
-		fprintf(stderr,"%s: Require at least two processors.", argv[0]);
+		fprintf(stderr, "%s: Require at least two processors.", argv[0]);
 		MPI_Abort(MPI_COMM_WORLD, __LINE__);
 	}
 
 	/* Only the master process handles the input and data initalization */
-	if (process_rank == 0) {
+	if (process_rank == 0)
+	{
 		build_table();
 
 		/* Get input from stdin */
@@ -71,7 +72,8 @@ int main(int argc, char *argv[])
 			   &weights[3]);
 		/* Get sequence 1 and convert to uppercase */
 		fscanf(stdin, "%s", seq1);
-		for (int i = 0; i < strlen(seq1); i++) {
+		for (int i = 0; i < strlen(seq1); i++)
+		{
 			if (seq1[i] >= 'a' && seq1[i] <= 'z')
 				seq1[i] = toupper(seq1[i]);
 		}
@@ -81,14 +83,15 @@ int main(int argc, char *argv[])
 		payload = (Payload *)malloc(sizeof(Payload) * num_of_sequences);
 		scores = (Score *)malloc(sizeof(Score) * num_of_sequences);
 		// work_sizes = (WorkSize *)malloc(sizeof(WorkSize) * num_of_sequences);
-		
+
 		/* Get the sequeneces for comparision and save into structs */
 		for (int i = 0; i < num_of_sequences; i++)
-		{	
+		{
 			strcpy(payload[i].seq1, seq1);
 			fscanf(stdin, "%s", payload[i].seq2);
 			payload[i].len = strlen(payload[i].seq2);
-			for (int j = 0; j < payload[j].len; j++) {
+			for (int j = 0; j < payload[j].len; j++)
+			{
 				if (payload[i].seq2[j] >= 'a' && payload[i].seq2[j] <= 'z')
 					payload[i].seq2[j] = toupper(payload[i].seq2[j]);
 			}
@@ -97,7 +100,6 @@ int main(int argc, char *argv[])
 			scores[i].offset = 0;
 			scores[i].alignment_score = 0;
 			scores[i].max_score = strlen(payload[i].seq2) * weights[0];
-
 		}
 	}
 
@@ -126,13 +128,13 @@ int main(int argc, char *argv[])
 	MPI_Type_create_struct(4, paylod_block_len, disp2, paylod_types, &PayloadMPIType);
 	MPI_Type_commit(&PayloadMPIType);
 
-
 	/* 	Broadcasting shared values for all MPI's processes */
 	MPI_Bcast(&num_of_sequences, 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
 	MPI_Bcast(&weights, WEIGHTS_NUM, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
 	MPI_Bcast(&chars_comparision, CHARS * CHARS, MPI_CHAR, MASTER_PROCESS, MPI_COMM_WORLD);
 	/* Allocation for other processes */
-	if (process_rank != 0) {
+	if (process_rank != 0)
+	{
 		payload = (Payload *)malloc(sizeof(Payload) * num_of_sequences);
 		scores = (Score *)malloc(sizeof(Score) * num_of_sequences);
 	}
@@ -141,12 +143,12 @@ int main(int argc, char *argv[])
 	MPI_Bcast(scores, num_of_sequences, ScoreMPIType, MASTER_PROCESS, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
 
-
-
 	/* Calculate score for each sequence, each processes takes a part of the max_offset. */
-	if (process_rank == 0) {
+	if (process_rank == 0)
+	{
 		Score tmp_score;
-		for (int i = 0; i < num_of_sequences; ++i) {
+		for (int i = 0; i < num_of_sequences; ++i)
+		{
 			/* Calculate offset for each sequence per process */
 			work_size = payload[i].max_offset / procceses_amount;
 			offset_remainder = payload[i].max_offset % procceses_amount;
@@ -158,25 +160,27 @@ int main(int argc, char *argv[])
 			/* Set start/end offsets */
 			process_start_offset = process_rank * work_size;
 			process_end_offset = process_start_offset + cuda_omp_work_size;
-			// process_end_offset = (process_rank + 1) * work_size;
 			omp_start_offset = process_end_offset;
 			omp_end_offset = omp_start_offset + cuda_omp_work_size + cuda_omp_offset_reminder;
-			
+
 			/* Master process to do his work size */
 			find_optimal_offset(&payload[i], &scores[i], process_start_offset, process_end_offset);
 
 			// find_optimal_offset(&payload[i], &scores[i], omp_start_offset, omp_end_offset);
 			find_optimal_offset_omp(&payload[i], &scores[i], omp_start_offset, omp_end_offset);
+
 			/* Recieve results from other processes and   */
 			for (int j = 1; j < procceses_amount; j++)
 			{
 				MPI_Recv(&tmp_score, 1, ScoreMPIType, j, RESULT_TAG, MPI_COMM_WORLD, &status);
 				compare_scores_and_swap(&tmp_score, &scores[i]);
 			}
-		}	
+		}
 	}
-	else {
-		for (int i = 0; i < num_of_sequences; i++) {
+	else
+	{
+		for (int i = 0; i < num_of_sequences; i++)
+		{
 			/* Calculate offset for each sequence per process */
 			work_size = payload[i].max_offset / procceses_amount;
 			offset_remainder = payload[i].max_offset % procceses_amount;
@@ -188,12 +192,12 @@ int main(int argc, char *argv[])
 			/* Set start/end offsets */
 			process_start_offset = process_rank * work_size;
 			process_end_offset = process_start_offset + cuda_omp_work_size;
-			// process_end_offset = (process_rank + 1) * work_size;
 			omp_start_offset = process_end_offset;
 			omp_end_offset = omp_start_offset + cuda_omp_work_size + cuda_omp_offset_reminder;
 
 			/* Last process handles offset reminder if exists */
-			if (process_rank == procceses_amount - 1 && offset_remainder != 0) {
+			if (process_rank == procceses_amount - 1 && offset_remainder != 0)
+			{
 				omp_end_offset += offset_remainder;
 			}
 
@@ -202,15 +206,15 @@ int main(int argc, char *argv[])
 			// find_optimal_offset(&payload[i], &scores[i], omp_start_offset, omp_end_offset);
 			find_optimal_offset_omp(&payload[i], &scores[i], omp_start_offset, omp_end_offset);
 
-
 			MPI_Send(&scores[i], 1, ScoreMPIType, MASTER_PROCESS, RESULT_TAG, MPI_COMM_WORLD);
-		}		
+		}
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
-	
+
 	/* Print Results */
-	if (process_rank == 0) {
+	if (process_rank == 0)
+	{
 		end = MPI_Wtime();
 		printf("Execution time is %f\n", end - start);
 		results_output(scores, num_of_sequences);
@@ -223,37 +227,34 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-
-Score* find_optimal_offset_omp(const Payload *source, Score *score, int start_offset, int end_offset)
+Score *find_optimal_offset_omp(const Payload *source, Score *score, int start_offset, int end_offset)
 {
-	Score *tmp = (Score*) malloc(sizeof(Score));
+	Score *tmp = (Score *)malloc(sizeof(Score));
 
 	/* Run first compare on source score and then compare to tmp scores */
 	compare(source, score);
 	score->offset = start_offset;
 	tmp = deep_copy_score(score, tmp);
-	
-	for (int i = start_offset; i <= end_offset - 1; ++i) {
+
+	for (int i = start_offset; i <= end_offset - 1; ++i)
+	{
 		tmp->offset = i;
 
 		/* for each hyphen offset find optimal */
 		#pragma omp parallel for default(none) firstprivate(tmp) shared(source, score, start_offset, end_offset)
-		for (int j = 1; j < source->len; ++j) {	
+		for (int j = 1; j < source->len; ++j)
+		{
 			tmp->hyphen_idx = j;
-			#pragma omp critical 
+			#pragma omp critical
 			{
 				compare(source, tmp);
-				if (tmp->alignment_score > score->alignment_score) {
-					score->offset = tmp->offset;
-					score->hyphen_idx = tmp->hyphen_idx;
-					score->alignment_score = tmp->alignment_score;
-					score->max_score = tmp->max_score;
-				}
+				compare_scores_and_swap(tmp, score);
 			}
-		}	
+		}
 	}
 	/* hyphen_idx = 0 -> means the hyphen is at the end of the string (e.g. ABC-) */
-	if (score->hyphen_idx == 0) {
+	if (score->hyphen_idx == 0)
+	{
 		score->hyphen_idx = source->len;
 	}
 
@@ -263,25 +264,28 @@ Score* find_optimal_offset_omp(const Payload *source, Score *score, int start_of
 
 Score *find_optimal_offset(const Payload *source, Score *score, int start_offset, int end_offset)
 {
-	Score *tmp = (Score*) malloc(sizeof(Score));
+	Score *tmp = (Score *)malloc(sizeof(Score));
 
 	/* Run first compare on source score and then compare to tmp scores */
-	compare(source, score); 
+	compare(source, score);
 	score->offset = start_offset;
 	tmp = deep_copy_score(score, tmp);
 
-	for (int i = start_offset; i <= end_offset - 1 && is_score_optimized(score); ++i) {
+	for (int i = start_offset; i <= end_offset - 1 && is_score_optimized(score); ++i)
+	{
 		tmp->offset = i;
 
 		/* for each hyphen offset find optimal */
-		for (int j = 1; j < source->len && is_score_optimized(score); ++j) {	
+		for (int j = 1; j < source->len && is_score_optimized(score); ++j)
+		{
 			tmp->hyphen_idx = j;
 			compare(source, tmp);
 			compare_scores_and_swap(tmp, score);
-		}	
+		}
 	}
 	/* hyphen_idx = 0 -> means the hyphen is at the end of the string (e.g. ABC-) */
-	if (score->hyphen_idx == 0) {
+	if (score->hyphen_idx == 0)
+	{
 		score->hyphen_idx = source->len;
 	}
 
@@ -312,7 +316,7 @@ Score *deep_copy_score(const Score *source, Score *dest)
 	return dest;
 }
 
-/* 
+/*
 The functions compares characters between the 2 sequences in the Payload,
 and calculates the alignment score for a sequence #2
 */
@@ -321,36 +325,39 @@ void compare(const Payload *payload, Score *score)
 	int passed_hypen_flag = 0;
 	int seq1_char, seq2_char;
 	score->alignment_score = 0;
-	for (int char_index = 0; char_index < payload->len; ++char_index) {
+	for (int char_index = 0; char_index < payload->len; ++char_index)
+	{
 		/* Convert char to ABC order index */
 		seq2_char = payload->seq2[char_index] - 'A';
-		if ((char_index == score->hyphen_idx && score->hyphen_idx != 0) || passed_hypen_flag) {
-			seq1_char = payload->seq1[char_index + 1 + score->offset] - 'A';	/* skip character if index is hyphen */
+		if ((char_index == score->hyphen_idx && score->hyphen_idx != 0) || passed_hypen_flag)
+		{
+			seq1_char = payload->seq1[char_index + 1 + score->offset] - 'A'; /* skip character if index is hyphen */
 			passed_hypen_flag = 1;
 		}
-		else {
+		else
+		{
 			seq1_char = payload->seq1[char_index + score->offset] - 'A';
 		}
 		/* check sign of characters */
-		switch (chars_comparision[seq1_char][seq2_char]) {
-			case '$':
-				score->alignment_score += weights[0];
-				break;
-			case '%':
-				score->alignment_score -= weights[1];
-				break;
-			case '#':
-				score->alignment_score -= weights[2];
-				break;
-			default:
-				score->alignment_score -= weights[3];
-				break;
+		switch (chars_comparision[seq1_char][seq2_char])
+		{
+		case '$':
+			score->alignment_score += weights[0];
+			break;
+		case '%':
+			score->alignment_score -= weights[1];
+			break;
+		case '#':
+			score->alignment_score -= weights[2];
+			break;
+		default:
+			score->alignment_score -= weights[3];
+			break;
 		}
 	}
 }
 
-
-/* 
+/*
 The functions builds a characters table with the size CHARS * CHARS and fills it with signs
 that corresponding to the rules of alignment score.
 - Two identical characters are marked with the sign '$'
@@ -359,19 +366,23 @@ that corresponding to the rules of alignment score.
 */
 void build_table()
 {
-	for (int row = 0; row < CHARS; ++row) {
-		for (int column = 0; column < CHARS; ++column) {
+	for (int row = 0; row < CHARS; ++row)
+	{
+		for (int column = 0; column < CHARS; ++column)
+		{
 			chars_comparision[row][column] = (row == column) ? '$' : ' ';
 		}
 	}
 
 	/* Fill first type groups characters */
-	for (int i = 0; i < FIRST_TYPE_GROUP_SIZE; ++i) {
+	for (int i = 0; i < FIRST_TYPE_GROUP_SIZE; ++i)
+	{
 		insert_string(first_type_groups[i], '%');
 	}
 
 	/* Fill second type groups characters */
-	for (int i = 0; i < SECOND_TYPE_GROUP_SIZE; ++i) {
+	for (int i = 0; i < SECOND_TYPE_GROUP_SIZE; ++i)
+	{
 		insert_string(second_type_groups[i], '#');
 	}
 }
